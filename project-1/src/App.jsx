62192a25abbc4b38f38b211f43f5ec846e8643ca
dev nayak10 <devprasannanayak@gmail.com>
2025-11-21 16:17:16 +0530

    content added

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,22 +1,10 @@
 import './App.css'
+import Navigation from './components/Navigation'
 function App() {
 
   return (
     <div>
-      <nav>
-        <div className="logo">
-          <img src="./images/brand_logo.png" alt="" />
-        </div>
-
-        <ul>
-          <li href='#'>Menu</li>
-          <li href='#'>Location</li>
-          <li href='#'>About</li>
-          <li href='#'>Contact</li>
-        </ul>
-
-        <button>Login</button>
-      </nav>
+      <Navigation />
     </div>
   )
 }
