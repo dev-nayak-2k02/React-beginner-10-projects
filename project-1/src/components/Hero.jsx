66f6976529ec9d0a7ee9d1ca27958e991bcd6448
dev nayak10 <devprasannanayak@gmail.com>
2025-11-21 16:23:53 +0530

    content added

--- project-1/src/components/Hero.jsx
+++ project-1/src/components/Hero.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <main>
-        
+    <main className="hero">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          SHOES.
+        </p>
+      </div>
+      <div className="hero-image"></div>
     </main>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
