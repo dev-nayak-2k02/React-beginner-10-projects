8ecf61484c76219c5a952e1880579232a187934a
dev nayak10 <devprasannanayak@gmail.com>
2025-11-21 15:39:46 +0530

    content added

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -6,6 +6,13 @@ function App() {
         <div className="logo">
           <img src="./images/brand_logo.png" alt="" />
         </div>
+        <ul>
+          <li href='#'>Menu</li>
+          <li href='#'>Location</li>
+          <li href='#'>About</li>
+          <li href='#'>Contact</li>
+        </ul>
+        <button>Login</button>
       </nav>
     </div>
   )
