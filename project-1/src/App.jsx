d14d2200fa60c68165b1386d091ae22b019e09ab
dev nayak10 <devprasannanayak@gmail.com>
2025-11-21 15:22:11 +0530

    content added

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
 
   return (
-    <>
-      <div>
-        hello world
-      </div>
-    </>
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="" alt="" />
+        </div>
+      </nav>
+    </div>
   )
 }
 
