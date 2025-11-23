f76c9eaab3fd574b4d78f959dcfab78ed180b657
dev nayak10 <devprasannanayak@gmail.com>
2025-11-23 15:17:53 +0530

    content added

--- project-1.2/src/components/Navigation.jsx
+++ project-1.2/src/components/Navigation.jsx
@@ -1,22 +1,28 @@
-import React from 'react'
+import React from "react";
 const Navigation = () => {
   return (
-    <div>
-        <nav className='main-nav-section'>
-            <div className='logo'>
-                <img src="./images/cover.png" alt="logo" />
-            </div>
+    <nav className="main-nav-section">
+      <div className="logo">
+        <img src="./images/cover.png" alt="logo" />
+      </div>
 
-            <ul>
-                <li><a href="#about">about</a></li>
-                <li><a href="#blog">blog</a></li>
-                <li><a href="#projects">projects</a></li>
-                <li><a href="#resume">resume</a></li>
-                <button></button>
-            </ul>
-        </nav>
-    </div>
-  )
-}
+      <ul>
+        <li>
+          <a href="#about">about</a>
+        </li>
+        <li>
+          <a href="#blog">blog</a>
+        </li>
+        <li>
+          <a href="#projects">projects</a>
+        </li>
+        <li>
+          <a href="#resume">resume</a>
+        </li>
+        <button><i class="fa-solid fa-ellipsis"></i></button>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
