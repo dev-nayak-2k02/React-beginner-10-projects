0359132de7b1a9934f4fd7592b401db123e9f348
dev nayak10 <devprasannanayak@gmail.com>
2025-11-23 14:04:20 +0530

    content added

--- project-1.2/src/components/Navigation.jsx
+++ project-1.2/src/components/Navigation.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const Navigation = () => {
   return (
     <div>
-        
+        <nav className='main-nav-section'>
+            <div className='logo'>
+                <img src="./images/profile-pic.avif" alt="logo" />
+            </div>
+
+            <div>
+                
+            </div>
+        </nav>
     </div>
   )
 }
