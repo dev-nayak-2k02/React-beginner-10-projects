408b6dbcd5c7af23946dd520bb1d1cf153d4a899
dev nayak10 <devprasannanayak@gmail.com>
2025-11-23 14:05:42 +0530

    content added

--- project-1.2/src/App.jsx
+++ project-1.2/src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero"
+import Navigation from "./components/Navigation"
 
 function App() {
   
   return (
     <>
+      <Navigation/>
       <Hero />
     </>
   )
