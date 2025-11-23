aa728b67dbc3865a9d3225636f4f485781c80bef
dev nayak10 <devprasannanayak@gmail.com>
2025-11-23 16:06:30 +0530

    content added

--- project-1.2/src/components/Hero.jsx
+++ project-1.2/src/components/Hero.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <></>
+    <main>
+      <img src="./images/profile-pic.avif" alt="" />
+      <h1></h1>
+      <p></p>
+      <button>get in touch</button>
+    </main>
   )
 }
 
