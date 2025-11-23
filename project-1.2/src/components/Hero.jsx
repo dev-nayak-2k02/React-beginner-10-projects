bf2a4bb6959e90f0579b4aeafb71b68bd3bdfd93
dev nayak10 <devprasannanayak@gmail.com>
2025-11-23 16:31:59 +0530

    content added

--- project-1.2/src/components/Hero.jsx
+++ project-1.2/src/components/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <main>
       <img src="./images/profile-pic.avif" alt="" />
-      <h1></h1>
-      <p></p>
+      <h1>Hi I'm solo, Product Designer and <br />Developer</h1>
+      <p>I am an developer proficient in React and and web technologies. From 2024, I have <br />been practicing my gig. I mke useful applications for the web. I enjoy <br />cooking, reading and travelling</p>
       <button>get in touch</button>
     </main>
   )
