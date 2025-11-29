48b25d475109f838d7eece040fab804b4a42f1e7
dev nayak10 <devprasannanayak@gmail.com>
2025-11-29 18:32:44 +0530

    content added

--- project-2/src/components/ContactForm/ContactForm.jsx
+++ project-2/src/components/ContactForm/ContactForm.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import styles from './ContactForm.module.css';
 import Button from '../Button/Button';
+import { MdCall } from "react-icons/md";
 import { MdMessage } from "react-icons/md";
 const ContactForm = () => {
   return (
     <section className={styles.container}>
         <div className={styles.contact_form}>
             <Button text='VIA SUPPORT CHAT' icon={<MdMessage fontSize='24px'/>}/>
-            <Button text='VIA CALL' icon={<MdMessage />}/>
+            <Button text='VIA CALL' icon={<MdCall fontSize='24px'/>}/>
         </div>
         <div className={styles.contact_image}></div>
     </section>
