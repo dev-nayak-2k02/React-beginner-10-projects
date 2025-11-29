f4c05110f92be801263d5769b36565f5f5830856
dev nayak10 <devprasannanayak@gmail.com>
2025-11-29 17:13:05 +0530

    content added

--- project-2/src/App.jsx
+++ project-2/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import ContactHeader from './components/Navigation/ContactHeader/ContactHeader'
 import Navigation from './components/Navigation/Navigation'
 function App() {
     return (
     <>
       <Navigation />
+      <ContactHeader />
     </>
   )
 }
