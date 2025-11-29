6996913aa9d51310c4634eea7b37afc919bc92f7
dev nayak10 <devprasannanayak@gmail.com>
2025-11-29 18:30:09 +0530

    content added

--- project-2/src/components/Button/Button.jsx
+++ project-2/src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Button = (props) => {
   return (
     <button className={styles.primary_btn}>
-        <MdMessage fontSize='24px'/>
+        {props.icon}
         {props.text}
     </button>
   )
--- project-2/src/components/ContactForm/ContactForm.jsx
+++ project-2/src/components/ContactForm/ContactForm.jsx
@@ -6,7 +6,8 @@ const ContactForm = () => {
   return (
     <section className={styles.container}>
         <div className={styles.contact_form}>
-            <Button text='VIA SUPPORT CHAT' icon={<MdMessage />}/>
+            <Button text='VIA SUPPORT CHAT' icon={<MdMessage fontSize='24px'/>}/>
+            <Button text='VIA CALL' icon={<MdMessage />}/>
         </div>
         <div className={styles.contact_image}></div>
     </section>
