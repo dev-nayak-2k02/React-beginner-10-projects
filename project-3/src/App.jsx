6f4b581c2371d978cedde7e69a00436a7f343249
dev nayak10 <devprasannanayak@gmail.com>
2025-12-14 16:52:38 +0530

    content-added

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Button>Click me i'm styled-component</Button>
+    </div>
   )
 }
 
 export default App
+
+const Button = styled.button`
+  background-color: black;
+  color: white;
+  padding: 10px;
+  border-radius: 4px;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+`
