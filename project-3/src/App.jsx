4d0d0cfb3e6674c051f65b925648baaa969cf461
dev nayak10 <devprasannanayak@gmail.com>
2025-12-14 17:00:59 +0530

    content-added

--- project-3/src/App.jsx
+++ project-3/src/App.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import styled from 'styled-components'
+import StartGame from './Components/StartGame'
 
 const App = () => {
   return (
     <div>
-      <Button>Click me i'm styled-component</Button>
+      <StartGame />
     </div>
   )
 }
 
 export default App
 
-const Button = styled.button`
-  background-color: black;
-  color: white;
-  padding: 10px;
-  border-radius: 4px;
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-`
