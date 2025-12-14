4ee438c24335408b0b6b68f3ebb3a5b50951c7c8
dev nayak10 <devprasannanayak@gmail.com>
2025-12-14 20:12:35 +0530

    content-added

--- project-3/src/Components/GamePlay.jsx
+++ project-3/src/Components/GamePlay.jsx
@@ -8,22 +8,28 @@ const GamePlay = () => {
   const [score, setScore] = useState(0);
   const [selectedNumber, setSelectedNumber] = useState();
   const [currentDice, setCurrentDice] = useState(1);
+  const [error, setError] = useState('')
 
   const generateRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min) + min);
   };
 
   const rollDice = () => {
+    if(!selectedNumber) {
+      setError('You have not selected any number')
+      return;
+    }
+    setError('')
+
     const randomNumber = generateRandomNumber(1, 7);
     setCurrentDice((prev) => randomNumber);
-
 
     if(selectedNumber == randomNumber){
       setScore(prev=> prev + randomNumber)
     } else {
       setScore(prev=> prev - 2)
     }
-
+    setSelectedNumber(undefined)
 
 
   };
@@ -33,6 +39,8 @@ const GamePlay = () => {
       <div className="top_section">
         <TotalScore score={score}/>
         <NumberSelector
+          error={error}
+          setError={setError}
           selectedNumber={selectedNumber}
           setSelectedNumber={setSelectedNumber}
         />
