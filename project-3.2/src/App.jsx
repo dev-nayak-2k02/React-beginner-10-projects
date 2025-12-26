f51e24839f54641ad61590f3da42035004fc81ab
dev nayak10 <devprasannanayak@gmail.com>
2025-12-26 19:07:28 +0530

    content added

--- project-3.2/src/App.jsx
+++ project-3.2/src/App.jsx
@@ -1,20 +1,30 @@
-import React, { useState } from 'react'
-import Background from './components/Background/Background'
+import React, { useState } from "react";
+import Background from "./components/Background/Background";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   let heroData = [
-    {text1:'Dive into', text2:'what you love'},
-    {text1:'Indulge', text2:'your passions'},
-    {text1:'Give in to', text2:'your passions'}
-  ]
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passions" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
 
-  const[heroCount, setHeroCount] = useState(2)
-  const[playStatus, setPlayStatus] = useState(false)
+  const [heroCount, setHeroCount] = useState(0);
+  const [playStatus, setPlayStatus] = useState(false);
   return (
     <div>
-      <Background heroCount={heroCount} playStatus={playStatus}/>
+      <Background heroCount={heroCount} playStatus={playStatus} />
+      <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
