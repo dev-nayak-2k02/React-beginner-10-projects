d30a2aedf2e63819daa04450c900c642f9b1f87d
dev nayak10 <devprasannanayak@gmail.com>
2026-01-13 17:37:22 +0530

    content added

--- project-4/app/src/App.jsx
+++ project-4/app/src/App.jsx
@@ -1,5 +1,24 @@
+import styled from 'styled-components'
+import React from 'react'
 const App = () => {
-  return <div>App</div>;
-};
+  return (
+    <MainContainer>
+      <TopContainer>
+        <div className='logo'>
+          <img src="/logo.svg" alt="" />
+        </div>
 
-export default App;
+        <div className="search">
+          <input type="text" placeholder='Search Food....'/>
+        </div>
+      </TopContainer>
+    </MainContainer>
+  )
+}
+
+export default App
+
+const MainContainer = styled.div``
+const TopContainer = styled.div`
+  
+`
